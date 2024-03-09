67250014bb895fceb32b9c634dcb1d7569a9fbc4
shuzk <2717155243@qq.com>
2024-03-09 12:45:48 +0800

    11

---
+++ shiro-day01-01authenticator/src/main/java/com/itheima/shiro/javassist/Person.java
@@ -0,0 +1,25 @@
+package com.itheima.shiro.javassist;
+
+public class Person {
+    private String name = "xiaoming";
+
+    public void setName(String var1) {
+        this.name = var1;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Person() {
+        this.name = "xiaohong";
+    }
+
+    public Person(String var1) {
+        this.name = var1;
+    }
+
+    public void printName() {
+        System.out.println(this.name);
+    }
+}
---
+++ shiro-day01-01authenticator/src/main/java/com/itheima/shiro/javassist/PersonI.java
@@ -0,0 +1,9 @@
+package com.itheima.shiro.javassist;
+
+public interface PersonI {
+    void setName(String name);
+
+    String getName();
+
+    void printName();
+}
